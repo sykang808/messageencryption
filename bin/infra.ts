0502d2bb546243cef52ce6ed8a74cd82ec1a1402
seyong <sykang@amazon.com>
2023-04-05 08:23:25 +0900

    fix bug

--- bin/infra.ts
+++ bin/infra.ts
@@ -2,13 +2,9 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 
-import { InfraStage } from '../lib/stage/infra-stage';
 import { CdkPipelineStack } from '../lib/cdk-pipeline-stack';
 
 const app = new cdk.App();
-new InfraStage(app, 'InfraStack', { 
-  
-});
 
 new CdkPipelineStack(app, 'CdkPipelineStack', {
   env: {
